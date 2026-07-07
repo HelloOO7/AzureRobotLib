431d2a20a0f4a89ccb9abf4a086e1df90091d0ac
Hello007-PC\Čeněk <cendarehor@gmail.com>
2026-07-07 15:44:05 +0200

    Add compatibility wrapper Robotabor2025 for Robotabor.java v2025.7 over v2026.1.

---
+++ Azure/src/Robotabor2025.java
@@ -0,0 +1,63 @@
+import lejos.nxt.*;
+
+/**
+ * EasyRobotLibrary.
+ *
+ * @author Dr. David (TM), Tomáš, Čeněk.
+ * @version 2026.1.compat
+ */
+public class Robotabor2025 extends Robotabor {
+	
+	public static void init_buggy(float prumer_kola, float rozchod_kol) {
+		initBuggy(prumer_kola, rozchod_kol);
+	}
+	
+	public static void init_buggy(float prumer_kola, float rozchod_kol, NXTRegMotor levy_motor,
+			NXTRegMotor pravy_motor) {
+		initBuggy(prumer_kola, rozchod_kol, levy_motor, pravy_motor);
+	}
+
+	public static int getb() {
+		return getButton();
+	}
+	
+	public static int readb() {
+		return readButton();
+	}
+	
+	public static int ms_time() {
+		return msTime();
+	}
+	
+	public static void ms_sleep(int milliseconds) {
+		msSleep(milliseconds);
+	}
+	
+	public boolean is_going() {
+		return isGoing();
+	}
+	
+	public float current_distance() {
+		return getCurrentDistance();
+	}
+	
+	public void calibrate_light(LightSensor ls, int uhel) {
+		calibrateBuggy(ls, uhel);
+	}
+	
+	public void find_track(float searchSpeed) {
+		findTrack(searchSpeed);
+	}
+	
+	public static void start_following(float p, float i, float d, float bsat, float wsat, float sdecay) {
+		startFollowing(p, i, d, bsat, wsat, sdecay);
+	}
+	
+	public static void stop_following() {
+		stopFollowing();
+	}
+	
+	public static void recalibrate_on_white() {
+		recalibrateOnWhite();
+	}
+}
